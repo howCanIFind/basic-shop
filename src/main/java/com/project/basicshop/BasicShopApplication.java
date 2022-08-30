0112fcdc60ba4cd2f5047e2ded44e240bb989a8b
howCanIFind <second9000@naver.com>
2022-08-30 16:28:05 +0900

    프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/com/project/basicshop/BasicShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.basicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/basicshop/BasicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.basicshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
