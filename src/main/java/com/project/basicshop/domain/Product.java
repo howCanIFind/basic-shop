13517a08357f7bb04834fb56f233c3dd2958c4aa
howCanIFind <second9000@naver.com>
2022-09-07 20:02:52 +0900

    Product Entity 추가
    Product Entity 추가

--- src/main/java/com/project/basicshop/domain/Product.java
+++ src/main/java/com/project/basicshop/domain/Product.java
@@ -1,4 +1,21 @@
 package com.project.basicshop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+
+    @Id @GeneratedValue
+    @Column
+    private Long id;
+
+    private String productName;
+
+    private String category;
+
+    private String conpanyName;
+
 }
