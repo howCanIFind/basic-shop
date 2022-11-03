84592a84069d4e3f422cd861c544ee9b8ed2343a
howCanIFind <second9000@naver.com>
2022-11-03 18:48:15 +0900

    Food 생성자 생성
    Food 생성자 생성

--- src/main/java/com/project/basicshop/domain/product/Food.java
+++ src/main/java/com/project/basicshop/domain/product/Food.java
@@ -2,8 +2,22 @@ package com.project.basicshop.domain.product;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 @DiscriminatorValue("F")
 public class Food extends Product{
+
+    private String areaOfProduction;
+
+    protected Food() {
+    }
+
+    public Food(String productName, String companyName, Integer price, Integer deliveryFee , String areaOfProduction) {
+        productName = productName;
+        companyName = companyName;
+        price = price;
+        deliveryFee = deliveryFee;
+        this.areaOfProduction = areaOfProduction;
+    }
 }
