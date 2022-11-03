26d15ba3d435b5c0e80c241afddb0f6a6770478b
howCanIFind <second9000@naver.com>
2022-11-03 18:48:34 +0900

    ITDevice 생성자 생성
    ITDevice 생성자 생성

--- src/main/java/com/project/basicshop/domain/product/ITDevice.java
+++ src/main/java/com/project/basicshop/domain/product/ITDevice.java
@@ -8,4 +8,15 @@ import javax.persistence.Entity;
 public class ITDevice extends Product{
 
     private String warrantyDate;
+
+    protected ITDevice() {
+    }
+
+    public ITDevice(String productName, String companyName, Integer price, Integer deliveryFee , String warrantyDate) {
+        productName = productName;
+        companyName = companyName;
+        price = price;
+        deliveryFee = deliveryFee;
+        this.warrantyDate = warrantyDate;
+    }
 }
