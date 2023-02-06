219db23b06173fc1dae0623782f555371e557c4f
howCanIFind <second9000@naver.com>
2023-02-06 23:41:06 +0900

    user entity 추가

--- src/main/java/com/project/basicshop/domain/User.java
+++ src/main/java/com/project/basicshop/domain/User.java
@@ -1,22 +1,28 @@
 package com.project.basicshop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class User {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
-    private String username;
+    @Column(nullable = false)
+    private String name;
 
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
 }
