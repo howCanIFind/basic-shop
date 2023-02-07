6394c99ccffb6067b70bbc71a4a29995842040bd
howCanIFind <second9000@naver.com>
2023-02-07 09:07:18 +0900

    loginUserArgumentResolver 및 webMvc 등록

--- src/main/java/com/project/basicshop/config/WebConfig.java
+++ src/main/java/com/project/basicshop/config/WebConfig.java
@@ -1,4 +1,21 @@
 package com.project.basicshop.config;
 
-public class WebConfig {
+import com.project.basicshop.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
 }
