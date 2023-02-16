4f6de88dde1dde24f4b97aa8339da010b068a84c
howCanIFind <second9000@naver.com>
2023-02-16 15:35:22 +0900

    게시글 전체 조회 추가

--- src/test/java/com/project/basicshop/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/project/basicshop/domain/product/ProductRepositoryTest.java
@@ -54,4 +54,40 @@ class ProductRepositoryTest {
         assertThat(findProduct.getCategory()).isEqualTo(savedProduct.getCategory());
         assertThat(findProduct.getPrice()).isEqualTo(savedProduct.getPrice());
     }
+
+    @Test
+    public void 게시글_전체_조회() {
+        // given
+        User user = User.builder()
+                .email("이메일")
+                .name("유저 이름")
+                .picture("사진")
+                .role(Role.USER)
+                .build();
+        User savedUser = userRepository.save(user);
+        Product product1 = Product.builder()
+                .name("상품 이름1")
+                .company("회사 이름1")
+                .category("카테고리1")
+                .price(10000L)
+                .user(savedUser)
+                .build();
+        Product product2 = Product.builder()
+                .name("상품 이름2")
+                .company("회사 이름2")
+                .category("카테고리2")
+                .price(10000L)
+                .user(savedUser)
+                .build();
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+
+
+        // when
+        List<Product> result = productRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+//        assertThat(result).contains(savedProduct1, savedProduct2);
+    }
 }
