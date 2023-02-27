a6649e3a89e3be857c4e6cfecb405a6b20e5abf6
howCanIFind <second9000@naver.com>
2023-02-27 15:14:56 +0900

    테스트 수정

--- src/test/java/com/project/basicshop/service/ProductServiceTest.java
+++ src/test/java/com/project/basicshop/service/ProductServiceTest.java
@@ -175,14 +175,4 @@ class ProductServiceTest {
         assertThat(productResponseDto.getCategory()).isEqualTo(productResponseDto.getCategory());
         assertThat(productResponseDto.getPrice()).isEqualTo(productResponseDto.getPrice());
     }
-
-    public void 상품삭제하기_test() {
-        // given
-
-        // stub
-
-        // when
-
-        // then
-    }
 }
